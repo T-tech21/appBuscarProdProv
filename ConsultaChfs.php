e49b979fce45f36c062d156c4d681631026ba7d4
T-tech21 <74573606+T-tech21@users.noreply.github.com>
2020-12-23 12:58:17 -0600

    Add files via upload

--- ConsultaChfs.php
+++ ConsultaChfs.php
@@ -39,19 +39,28 @@ $result=mysqli_query($sqln,$sku) or die(mysqli_error());
         <form method="GET">
   <div class="form-row align-items-center">
     <div class="col-auto">
-      <label class="sr-only" for="inlineFormInput">Inventario</label>
+      <label class="sr-only" for="inlineFormInput">Pedidos</label>
       <input name="Consulta Producto" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese Sku">  
-   if(isset($_GET["ConsultaCatalogo"])){                  
-$sqln=mysqli_query($conn, "SELECT Sku,Nombre,Tamaño,PrecioCosto,PrecioLocal,PrecioForaneo,PrecioBarra FROM catalogolars); /* WHERE catalogolars.sku LIKE '%$pbu%' ");*/
-  
-$result=mysqli_query($sqln,$sku) or die(mysqli_error());
-}
-?>
+   
         
         /*verificar si es una opcion el IdCliente en lugar del nombre para consulta de personal*/
         
-        //*consulta de pedido
+        /*consulta de pedido*/
         
+        <?php
+include('conexion.php');
+$conn = new mysqli($localhost, $usuario, $password, $Control);
+if ($conn->connect_error) {
+    die("la conexión ha fallado: " . $conn->connect_error);
+}
+if(isset($_GET["consultaPedido"])){
+$pbu=$_newproductoT["sku"];	
+	}      
+       <form method="GET">
+  <div class="form-row align-items-center">
+    <div class="col-auto">
+      <label class="sr-only" for="inlineFormInput">Inventario</label>
+      <input name="ConsultaPedido" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese Sku">  
         if(isset($_GET["Pedido"])){                  
 $sqln=mysqli_query($conn, "SELECT IdCliente,Folio,StatusPedido,VerificacionPedido,Total,FechaPedido,Pedido,PersonaAutoriza FROM Pedido); /* WHERE catalogolars.sku LIKE '%$pbu%' ");*/
   
@@ -59,44 +68,42 @@ $result=mysqli_query($sqln,$sku) or die(mysqli_error());
 }
 ?>
         
-       <form method="INSERT">
+       <form method="GET">
   <div class="form-row align-items-center">
     <div class="col-auto">
       <label class="sr-only" for="inlineFormInput">Producto</label>
-      <input name="IngProductosVendidos" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese Productos Vendidos">  
+      <input name="IngFolioPedido" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese  Folio de Pedido">  
         
-        if(isset($_INSERT["IngProdAdVendidos"])){                  
-$sqln=mysqli_query($conn, "INSERT Sku,Nombre,Tamaño,Descripcion,Marca,Proveedor,Categoria,ClaveProdServ,ClaveUnidad,Cantidad,Total,AreaVenta FROM VentasPorDia); /* WHERE catalogolars.sku LIKE '%$sku%' ");*/
-  
-$result=mysqli_query($sqln,$pbu) or die(mysqli_error());
+        
+   <?php
+include('conexion.php');
+$conn = new mysqli($localhost, $usuario, $password, $Control);
+if ($conn->connect_error) {
+    die("la conexión ha fallado: " . $conn->connect_error);
 }
-?>
-        
-       <form method="INSERT">
-  <div class="form-row align-items-center">
-    <div class="col-auto">
-      <label class="sr-only" for="inlineFormInput">Producto</label>
-      <input name="IngProductosVendidos" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese Productos Vendidos">  
- 
-       //*Envio por correo de ventas por dia, copias
-       
-         if(isset($_INSERT["Pedido"])){                  
-$sqln=mysqli_query($conn, "INSERT IdCliente,Folio,StatusPedido,VerificacionPedido,Total,FechaPedido,Pedido,PersonaAutoriza FROM Pedido); /* WHERE catalogolars.sku LIKE '%$pbu%' ");*/
+if(isset($_INSERT["IngProdAdVendidos"])){
+$pbu=$_IngProdAdVendidosT["sku"];	
+	}
+    //*incluir el insert a la consulta
+if(isset($_INSERT["VentasPorDia"])){                  
+$sqln=mysqli_query($conn, "INSERT Sku,Nombre,Tamaño,Descripcion,Marca,Proveedor,Categoria,ClaveProdServ,ClaveUnidad,Cantidad,Total,AreaVenta FROM VentasPorDia); /* WHERE catalogolars.sku LIKE '%$sku%' ");*/
   
 $result=mysqli_query($sqln,$sku) or die(mysqli_error());
 }
 ?>
-       
-        
-  <form method="INSERT">
-  <div class="form-row align-items-center">
-    <div class="col-auto">
-      <label class="sr-only" for="inlineFormInput">Pedido</label>
-      <input name="IdCliente" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese IdCliente">  
-       <input name="Folio" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingrese Folio">  
-        <input name="FechaPedido" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="FechaPedido"> 
-        <input name="Pedido" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Pedido"> 
-        
-        //*Envio de notificacion por correo electronico si el cliente se registro, copia a el, a almacen, a administracion
-       </nav>
-   </div>
+         <?php
+include('conexion.php');
+$conn = new mysqli($localhost, $usuario, $password, $Control);
+if ($conn->connect_error) {
+    die("la conexión ha fallado: " . $conn->connect_error);
+}
+if(isset($_INSERT["IngresodePedido"])){
+$pbu=$_IngProdAdVendidosT["sku"];	
+	}
+    
+if(isset($_INSERT["IngresodePedido"])){                  
+$sqln=mysqli_query($conn, "INSERT IdCliente,Folio,Total,FechaPedido,Pedido FROM Pedido); /* WHERE catalogolars.sku LIKE '%$sku%' ");*/
+  
+$result=mysqli_query($sqln,$sku) or die(mysqli_error());
+}
+?>
---
+++ Index2.vbhtml
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+ <link rel="stylesheet" href="style.css" >
+
+<div>
+      <h1>Por el momento estamos en mantenimiento, ingresa mas tarde</h1>
+
+<div>
+	<img source= "">
+<img src="lars.png.png">
+
+</div>
+
+</html>
