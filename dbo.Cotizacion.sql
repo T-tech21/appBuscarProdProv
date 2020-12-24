a6f5436e34c9d8e3b3da35d96a6eaa94d8d5b098
T-tech21 <74573606+T-tech21@users.noreply.github.com>
2020-12-24 12:42:59 -0600

    Add files via upload

---
+++ dbo.Cotizacion.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Productos]
+(
+	[CodigoAlterno] VARCHAR(MAX) NOT NULL PRIMARY KEY, 
+    [Sku] VARCHAR(MAX) NULL, 
+    [Descripcion] VARCHAR(MAX) NULL, 
+    [Precio1] FLOAT NULL, 
+    [Existencia] INT NULL, 
+    [Nombre] VARCHAR(MAX) NULL, 
+    [Tamaño] VARCHAR(MAX) NULL, 
+    [PrecioLocal] FLOAT NULL
+)
